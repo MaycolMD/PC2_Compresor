2b1a06a5523b1d93087ff63c61d475e60d0632de
nm000 <mendozanatalia0827@gmail.com>
2023-05-09 12:41:59 -0500

    Deleted compressor not parallelized

--- descompresor.py
+++
@@ -1,5 +0,0 @@
-from mainDecompressor import decompress
-
-file_path_source2 = 'comprimido.elmejorprofesor'
-file_path_destination2 = 'descomprimido-elmejorprofesor.txt'
-decompressTask = decompress(file_path_source2, file_path_destination2)
